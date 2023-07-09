41592492d1c93dfb7cbdaf1cfcf7a31d045f3856
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-07-09 17:41:28 -0400

    feat: add type aliases for NextFunction of nestjs middlewares

--- src/entrypoints/nestjs-platform-express.d.ts
+++ src/entrypoints/nestjs-platform-express.d.ts
@@ -1,13 +1,18 @@
-import type { Request, Response } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 declare module '@nestjs/platform-express' {
   /**
    * An alias to `Express.Request` (from `@types/express`).
-   * @author `nestjs-platform-expresss`
+   * @author `nestjs-types`
    */
   export type RequestExpress = Request
   /**
    * An alias to `Express.Response` (from `@types/express`).
-   * @author `nestjs-platform-expresss`
+   * @author `nestjs-types`
    */
   export type ResponseExpress = Response
+  /**
+   * An alias to `Express.NextFunction` (from `@types/express`).
+   * @author `nestjs-types`
+   */
+  export type NextFunctionExpress = NextFunction
 }
--- src/entrypoints/nestjs-platform-fastify.d.ts
+++ src/entrypoints/nestjs-platform-fastify.d.ts
@@ -2,12 +2,16 @@ import type { FastifyRequest, FastifyReply } from 'fastify'
 declare module '@nestjs/platform-fastify' {
   /**
    * An alias to `Fastify.FastifyRequest` (from `fastify`).
-   * @author `nestjs-platform-expresss`
+   * @author `nestjs-types`
    */
   export type RequestFastify = FastifyRequest
   /**
    * An alias to `Fastify.FastifyReply` (from `fastify`).
-   * @author `nestjs-platform-expresss`
+   * @author `nestjs-types`
    */
   export type ResponseFastify = FastifyReply
+  /**
+   * @author `nestjs-platform-expresss`
+   */
+  export type NextFunctionFastify = () => void
 }
